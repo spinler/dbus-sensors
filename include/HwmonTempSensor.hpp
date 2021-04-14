29c7521b4e978725cedf55b71562c16c67f25955
Matt Spinler <spinler@us.ibm.com>
2021-04-14 10:04:58 -0500

    hwmontemp: Create event logs for failed reads
    When a sensor has hit enough consecutive read fails that it gets set to
    nonfunctional with a sensor value of NaN, create an event log for it
    with the I2C bus, address, sensor name, and other details included in
    the event log.
    
    Only create event logs when power is on, and only create one event log
    per hardware device per power cycle.  i.e. if a temp sensor has two
    temp*_input files and both fail, only create an event log for one of
    them.
    
    Only one event log is created per power cycle so there is no flooding of
    event logs for flakey sensors that go in and out of readability.
    
    When power is switched on, clear the history of fails, and look for any
    sensors that are currently failed and create event logs for them.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: I97ce0583ed9bc82b357b667fc631fa2c8b0d02a3

--- include/HwmonTempSensor.hpp
+++ include/HwmonTempSensor.hpp
@@ -24,6 +24,12 @@ class HwmonTempSensor :
     ~HwmonTempSensor() override;
     void setupRead(void);
 
+    // Clears the history of failed reads
+    static void clearFailedDevices();
+
+    // Create an event log for a failed read
+    void createEventLog();
+
   private:
     sdbusplus::asio::object_server& objServer;
     boost::asio::posix::stream_descriptor inputDev;
@@ -33,6 +39,12 @@ class HwmonTempSensor :
     unsigned int sensorPollMs;
     size_t bus;
     size_t address;
+
+    // pair<bus, address> of devices with logged failed reads
+    static std::vector<std::pair<size_t, size_t>> failedDevices;
+
+    // The error code from the last HW access for the sensor.
+    boost::system::error_code errorCode;
 
     void handleResponse(const boost::system::error_code& err);
     void checkThresholds(void) override;
