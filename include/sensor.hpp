ad9b7d371fc993122d38ec348518766d37933c77
Matt Spinler <spinler@us.ibm.com>
2021-04-14 10:04:58 -0500

    hwmontemp: Create event logs for failed reads
    When a sensor has hit enough consecutive read fails that it gets set to
    nonfunctional with a sensor value of NaN, create an event log for it
    with the I2C bus, address, sensor name, and other details included in
    the event log.
    
    Only create event logs when power is on, and only create one event log
    per hardware device per power cycle.  i.e. if a temp sensor has two
    temp*_input files and both fail, only create an event log for one of
    them.
    
    Only one event log is created per power cycle so there is no flooding of
    event logs for flakey sensors that go in and out of readability.
    
    When power is switched on, clear the history of fails, and look for any
    sensors that are currently failed and create event logs for them.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: I97ce0583ed9bc82b357b667fc631fa2c8b0d02a3

--- include/sensor.hpp
+++ include/sensor.hpp
@@ -363,17 +363,18 @@ struct Sensor
         }
     }
 
-    void incrementError()
+    // Return true on the call that made the sensor go nonfunctional.
+    bool incrementError()
     {
         if (!readingStateGood())
         {
             markAvailable(false);
-            return;
+            return false;
         }
 
         if (errCount >= errorThreshold)
         {
-            return;
+            return false;
         }
 
         errCount++;
@@ -381,7 +382,9 @@ struct Sensor
         {
             std::cerr << "Sensor " << name << " reading error!\n";
             markFunctional(false);
+            return true;
         }
+        return false;
     }
 
     void updateValue(const double& newValue)
