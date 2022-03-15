9ee780e16d0d8ae901d5f29bbaa35eb96420ac28
Andrew Jeffery <andrew@aj.id.au>
2022-03-15 21:44:44 +1030

    sensor: Add inError() method
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Change-Id: I562505e0ce134f5a6978338e300615e0f9428b27

--- include/sensor.hpp
+++ include/sensor.hpp
@@ -477,6 +477,11 @@ struct Sensor
         return errCount >= errorThreshold;
     }
 
+    bool inError()
+    {
+        return errCount >= errorThreshold;
+    }
+
     void updateValue(const double& newValue)
     {
         // Ignore if overriding is enabled
